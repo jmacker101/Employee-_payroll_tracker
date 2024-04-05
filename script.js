eb06bbb7cfacaec0fe17dca9c15bf7597d446a0c
jmacker101 <jm470t@gmail.com>
2024-04-04 23:17:33 -0400

    first part finished 2nd part on its way

--- script.js
+++ script.js
@@ -2,9 +2,33 @@
 const addEmployeesBtn = document.querySelector('#add-employees-btn');
 
 // Collect employee data
-const collectEmployees = function() {
+const collectEmployees = function () {
   // TODO: Get user input to create and return an array of employee objects
-}
+  let decision = true;
+
+  const allEmployees = [];
+
+  while (decision) {
+    const firstNameCell = prompt("Enter Employee First Name");
+    const lastNameCell = prompt("Enter Employee Last Name");
+    let salaryCell = prompt("Enter Employee's Salary");
+    if (isNaN(salaryCell) || salaryCell === null) {
+      salaryCell = 0;
+    }
+    let employee = {
+      firstName: firstNameCell,
+      lastName: lastNameCell,
+      salary: parseFloat(salaryCell),
+    }
+
+      decision = confirm("Add another employee?");
+      allEmployees.push(employees)
+
+
+  } 
+return allEmployees
+} 
+
 
 // Display the average salary
 const displayAverageSalary = function(employeesArray) {
@@ -81,3 +105,54 @@ const trackEmployeeData = function() {
 
 // Add event listener to 'Add Employees' button
 addEmployeesBtn.addEventListener('click', trackEmployeeData);
+
+
+// Get a reference to the #add-employees-btn element
+const addEmployeesBtn = document.querySelector('#add-employees-btn');
+
+// Collect employee data
+const collectEmployees = function() {
+ // TODO: Get user input to create and return an array of employee objects
+}
+
+// Display the average salary
+const displayAverageSalary = function(employeesArray) {
+ // TODO: Calculate and display the average salary
+}
+
+// Select a random employee
+const getRandomEmployee = function(employeesArray) {
+ // TODO: Select and display a random employee
+}
+
+/*
+  ====================
+  STARTER CODE
+  Do not modify any of the code below this line:
+*/
+
+// Display employee data in an HTML table
+const displayEmployees = function(employeesArray) {
+  // Get the employee table
+  const employeeTable = document.querySelector('#employee-table');
+
+  // Clear the employee table
+  employeeTable.innerHTML = '';
+
+  // Loop through the employee data and create a row for each employee
+  for (let i = 0; i < employeesArray.length; i++) {
+    const currentEmployee = employeesArray[i];
+
+    const newTableRow = document.createElement("tr");
+
+    const firstNameCell = document.createElement("td");
+    firstNameCell.textContent = currentEmployee.firstName;
+    newTableRow.append(firstNameCell);
+
+    const lastNameCell = document.createElement("td");
+    lastNameCell.textContent = currentEmployee.lastName;
+    newTableRow.append(lastNameCell);
+
+ const salaryCell = document.createElement("td");
+ // Format the salary as currency
+   salaryCell.textContent = currentEmployee.salary.toLocaleString("en-US",{
